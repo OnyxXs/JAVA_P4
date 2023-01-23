8844aac006fdd28a3ebfcc0ae1495ce215538152
justoverfr <justover.fr@gmail.com>
2023-01-23 17:40:35 +0100

    miam

--- src/model/IA.java
+++ src/model/IA.java
@@ -1,5 +1,17 @@
 package model;
 
 public class IA {
+    private int difficulty;
 
+    public IA(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
 }
--- src/model/Player.java
+++ src/model/Player.java
@@ -1,17 +1,37 @@
 package model;
 
 public class Player {
-    private Pawn pawn;
+    private String name;
+    private String color;
+    private char symbol;
 
-    public Player(Pawn pawn) {
-        this.pawn = pawn;
+    public Player(String name, String color, char symbol) {
+        this.name = name;
+        this.color = color;
+        this.symbol = symbol;
     }
 
-    public Pawn getPawn() {
-        return pawn;
+    public String getName() {
+        return name;
     }
 
-    public void setPawn(Pawn pawn) {
-        this.pawn = pawn;
+    public String getColor() {
+        return color;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setSymbol(char symbol) {
+        this.symbol = symbol;
     }
 }
