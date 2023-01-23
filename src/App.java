1564ce789d5111660683fcbf450679839743198c
OnyxXs <timeo.donnaes@gmail.com>
2023-01-23 15:14:33 +0100

    update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        commetuveux();
+    }
+
+    public static void commetuveux(){
+        System.out.println("  1   2   3   4   5   6   7");
+        for (int ligne=0; ligne < 11; ligne++){
+            if (ligne%2 == 0){
+                System.out.println("|   |   |   |   |   |   |   |");
+            }
+            else{
+                System.out.println("|---+---+---+---+---+---+---|");
+            }
+        }
+        System.out.println("\\===========================/");
     }
 }
