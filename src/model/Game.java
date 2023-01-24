63f4552820b38aa9c977d20be6e211e6a3f79ba3
nom2 <113614747+Stanewolfer@users.noreply.github.com>
2023-01-24 10:52:12 +0100

    Update Game.java

--- src/model/Game.java
+++ src/model/Game.java
@@ -10,11 +10,8 @@ public class Game {
     private boolean isOver = false;
 
     public Game(Player player1, Player player2) {
-<<<<<<< Updated upstream
-=======
 
 
->>>>>>> Stashed changes
         this.player1 = player1;
         this.player2 = player2;
         currentPlayer = player1;
