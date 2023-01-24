9e441759905bd0c279a28a0f496cb79ff6ae22f2
OnyxXs <timeo.donnaes@gmail.com>
2023-01-24 11:20:18 +0100

    update Player.java

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,48 +1,83 @@
 package model;
 
+/**
+ * Classe publique représentant le joueur
+ */
 public class Player {
     private String name;
     private String color;
     private char symbol;
     private int number;
 
+    /**
+     * @param name le nom du joeur
+     * @param color la couleur du joueur
+     * @param symbol l'icone en jeu du joueur
+     */
     public Player(String name, String color, char symbol) {
         this.name = name;
         this.color = color;
         this.symbol = symbol;
     }
 
+    /**
+     * Crée un nouveau joueur
+     */
     public Player() {
     }
 
+    /**
+     * récupère et retourne le nom du joueur
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * récupère et retourne la couleur du joueur
+     */
     public String getColor() {
         return color;
     }
 
+    /**
+     * récupère et retourne l'icone du joueur
+     */
     public char getSymbol() {
         return symbol;
     }
 
+    /**
+     * récupère et retourne le numéro du joueur
+     */
     public int getNumber() {
         return number;
     }
 
+    /**
+     * créer un nom de joeur
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * créer une couleur de joueur
+     */
     public void setColor(String color) {
         this.color = color;
     }
 
+    /**
+     * créer un symbole de joueur
+     */
     public void setSymbol(char symbol) {
         this.symbol = symbol;
     }
 
+    /**
+     * créer le numéro du joueur
+     */
     public void setNumber(int number) {
         this.number = number;
     }
